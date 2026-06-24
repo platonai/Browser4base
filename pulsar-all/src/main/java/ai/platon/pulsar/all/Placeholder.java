d70f57399455aa38f3a80b1cbd0fdbf0cb7fe802
platon.AI <galaxyeye@live.cn>
2026-06-24 17:04:22 +0800

    fix: add placeholder class to generate source and doc jars for maven deploy

---
+++ pulsar-all/src/main/java/ai/platon/pulsar/all/Placeholder.java
@@ -0,0 +1,13 @@
+package ai.platon.pulsar.all;
+
+/**
+ * A placeholder class used just to generate source jars and doc jars so passes maven deploy checking policy.
+ * */
+public class Placeholder {
+    /**
+     * Show the help message of this module
+     * */
+    public String help() {
+        return "This is a placeholder to fix maven deploy problem";
+    }
+}
